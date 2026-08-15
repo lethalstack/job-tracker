b4234a604c4d131c6bf56a52311c1725a0efe8b5
Subhash Boina <subhashsubbu8374@gmail.com>
2026-08-15 17:03:15 +0530

    Remove login redirect message

--- extensions.py
+++ extensions.py
@@ -5,5 +5,3 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
-login_manager.login_message = "Please log in to access this page."
-login_manager.login_message_category = "info"
